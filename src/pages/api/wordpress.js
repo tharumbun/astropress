df1c4c42e40950fabc3cf2e1d584301829553ef7
kokitree <hello@kokitree.com>
2025-03-22 23:36:37 +0700

    api-route

---
+++ src/pages/api/wordpress.js
@@ -0,0 +1,5 @@
+export async function get() {
+  const res = await fetch('https://wp.phnompenhreview.com/wp-json/wp/v2/posts');
+  const posts = await res.json();
+  return new Response(JSON.stringify(posts));
+}
