9816d65802d1936eb5b19e4dd8af21bf342e1334
kokitree <hello@kokitree.com>
2025-03-23 13:10:55 +0700

    apifix

--- src/pages/api/wordpress.js
+++ src/pages/api/wordpress.js
@@ -1,3 +1,5 @@
+import { fetchWordPressPosts } from "../../lib/fetchWordPress.ts";
+
 export async function get() {
   const res = await fetch('https://wp.phnompenhreview.com/wp-json/wp/v2/posts');
   const posts = await res.json();
